c6fbe8db4e1d45f2eb45e19bb9246986c473ad16
Bill Tozier <bill@vagueinnovation.com>
2015-06-18 07:02:30 -0400

    doing some crossover

--- spikes/ordinal-tableau-spike.rb
+++ spikes/ordinal-tableau-spike.rb
@@ -1,10 +1,12 @@
+# exploring a slight extension of the max-ones problem
 
+require 'pp'
 
 @number_of_individuals = 1000
 @number_of_rubrics = 200
 
 tableau = @number_of_rubrics.times.collect do |i|
-  @number_of_individuals.times.collect {Random.rand(@number_of_individuals)}
+  (0...@number_of_individuals).to_a.shuffle  # @number_of_individuals.times.collect {Random.rand(@number_of_individuals)}
 end
 
 @tt = tableau.transpose
@@ -20,6 +22,7 @@ def tournament_select(tableau,tourney_size)
   return @tt.find_index (tourney.find_all {|individual| total_error(individual) == min_error}).sample
 end
 
+
 def lexicase_select(tableau)
   filter_order = (0...@number_of_rubrics).to_a.shuffle
   candidates = (0...@number_of_individuals).to_a
@@ -34,19 +37,47 @@ def lexicase_select(tableau)
   return @tt.find_index(scratch.sample)
 end
 
-
-
 # puts @tt.inspect
 
-tourney_histogram = Hash.new(0)
-tourney_winners = 1000.times do
-  tourney_histogram[tournament_select(tableau,7)] += 1
+# tourney_histogram = Hash.new(0)
+# tourney_winners = 5000.times do
+#   tourney_histogram[tournament_select(tableau,7)] += 1
+# end
+
+# lexi_histogram = Hash.new(0)
+# lexi_winners = 5000.times.collect do
+#   lexi_histogram[lexicase_select(tableau)] += 1
+# end
+
+# puts "Tournament (7): #{tourney_histogram.length}"
+# puts "Lexicase: #{lexi_histogram.length}"
+
+def crossover(mom,dad,prob=0.05)
+  baby = []
+  from_mom = true
+  mom.each_with_index do |token,idx|
+    baby.push (from_mom ? mom[idx] : dad[idx])
+    from_mom = !from_mom if Random.rand() < prob
+  end
+  return baby
 end
 
-lexi_histogram = Hash.new(0)
-lexi_winners = 1000.times.collect do
-  lexi_histogram[lexicase_select(tableau)] += 1
+# m = (1..30).to_a
+# d = (31..60).to_a
+# puts crossover(m,d,0.1).inspect
+
+def next_tableau(tableau)
+  result = []
+  @number_of_individuals.times do
+    mom = tableau[lexicase_select(tableau)]
+    dad = tableau[lexicase_select(tableau)]
+    baby = crossover(mom,dad)
+    result << baby
+  end
+  return result
 end
 
-puts "Tournament (7): #{tourney_histogram.length}"
-puts "Lexicase: #{lexi_histogram.length}"
+30.times do 
+  pp @tt
+  @tt = next_tableau(@tt)
+end
